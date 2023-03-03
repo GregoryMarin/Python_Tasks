6b4c9e02b953195c605fb552a01a8a5da1aef8de
Gregory <gpost774@gmail.com>
2023-03-03 16:58:15 +0300

    Commited

---
+++ StepikTasks/test9.py
@@ -0,0 +1,16 @@
+# Дополните приведенный код, используя форматирование строк с помощью метода format,
+# так чтобы он вывел текст:
+# «In 2010, someone paid 10k Bitcoin for two pizzas.» (без кавычек).
+
+# year = 2010
+# price = '10k'
+# currency = 'Bitcoin'
+# s = 'In {0}, someone paid {1} {2} for two pizzas.'.format(year, price, currency)
+#
+# print(s)
+
+year = 2010
+amount = '10K'
+currency = 'Bitcoin'
+
+print(f'In {year}, someone paid {amount} {currency} for two pizzas.')
