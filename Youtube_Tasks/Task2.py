e909950905dcc403b55f20b3f0b2c6e8b2626db3
Gregory <gpost774@gmail.com>
2023-03-11 21:24:01 +0300

    Commited

---
+++ Youtube_Tasks/Task2.py
@@ -0,0 +1,10 @@
+class Building:
+    year: None
+    city: None
+    
+    def __init__(self, year, city):
+        self.year = year
+        self.city = city
+        
+    def get_info(self):
+        print("Year:", self.year, "City:", self.city)
