2b3805e72462abd4cf7b67bce5e0ab9840a09b55
Gregory <gpost774@gmail.com>
2023-03-11 22:03:55 +0300

    Commited

--- GBtasks/task3.py
+++ GBtasks/task3.py
@@ -9,4 +9,4 @@
 import math
 max_distance = 700
 fact_distance = int(input("Please write the distance: "))
-print(f"You need {math.ceil(fact_distance / max_distance)} days for this distance, ")
+print(f"You need {math.ceil(fact_distance / max_distance)} day(s) for this distance.")
