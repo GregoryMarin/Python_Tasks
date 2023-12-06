056b322e9f4beb5997edc866e584f94f764c317f
Gregory <gpost774@gmail.com>
2023-12-05 23:38:48 -0800

    Commited few more tasks :

--- StepicAutotests/Autotests/task_10.py
+++ StepicAutotests/Autotests/task_10.py
@@ -1,28 +1,29 @@
 import time
 import math
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-link = "http://suninjuly.github.io/alert_accept.html"
 
 def calc(x):
   return str(math.log(abs(12*math.sin(int(x)))))
 
 
+link = "http://suninjuly.github.io/redirect_accept.html"
+
 br = webdriver.Chrome()
 br.get(link)
 
-but1 = br.find_element(By.XPATH, '//button').click()
+buttom1 = br.find_element(By.TAG_NAME, "button").click()
 
-confirm = br.switch_to.alert
-confirm.accept()
+
+new_window = br.window_handles[1]
+first_window = br.window_handles[0]
+br.switch_to.window(new_window)
 
 x = br.find_element(By.ID, "input_value").text
 res = calc(x)
 
 answer = br.find_element(By.ID, "answer").send_keys(res)
-
 button = br.find_element(By.XPATH, '//button').click()
 
 time.sleep(20)
