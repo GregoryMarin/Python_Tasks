b18039a25e34e5b74c177c9d2205befc5e4f8f38
Gregory <gpost774@gmail.com>
2024-01-14 22:40:44 -0800

    Commited

--- StepicAutotests/Autotests/ex_8.py
+++ StepicAutotests/Autotests/ex_8.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 browser = webdriver.Chrome()
 # говорим WebDriver ждать все элементы в течение 5 секунд
-# browser.implicitly_wait(5)
+browser.implicitly_wait(5)
 
 browser.get("http://suninjuly.github.io/wait2.html")
 
--- StepicAutotests/Autotests/task_11.py
+++ StepicAutotests/Autotests/task_11.py
@@ -3,20 +3,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import math
+
 link = "http://suninjuly.github.io/explicit_wait2.html"
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
 
 browser = webdriver.Chrome()
 browser.get(link)
 
-# В этой задаче вам нужно написать программу, которая будет выполнять следующий сценарий:
-    # Открыть страницу http://suninjuly.github.io/explicit_wait2.html
-    # Дождаться, когда цена дома уменьшится до $100 (ожидание нужно установить не меньше 12 секунд)
-    # Нажать на кнопку "Book"
-    # Решить уже известную нам математическую задачу (используйте ранее написанный код) и отправить решение
+# В этой задаче вам нужно написать программу, которая будет выполнять следующий сценарий
+# Открыть страницу http://suninjuly.github.io/explicit_wait2.html
+# Дождаться, когда цена дома уменьшится до $100 (ожидание нужно установить не меньше 12 секунд)
+# Нажать на кнопку "Book"
+# Решить уже известную нам математическую задачу (используйте ранее написанный код) и отправить решение
 
-button = browser.find_element(By.ID, "button")
-WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "100"))
-button.click()
+price = WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "100"))
+button = browser.find_element(By.ID, "book").click()
+browser.implicitly_wait(2)
+
+x = browser.find_element(By.ID, "input_value").text
+res = calc(x)
+
+answer = browser.find_element(By.ID, "answer").send_keys(res)
+buton2 = browser.find_element(By.ID, "solve").click()
 
 time.sleep(10)
 browser.quit()
